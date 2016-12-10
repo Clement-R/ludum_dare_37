704f0721e3d30bb6091f4e2289deabb21c3fff26
C.Rondeau <rondeau.clement@gmail.com>
2016-12-10 16:34:07 +0100

    Setup personal scene and start to work on enemy movement

---
+++ Assets/Script/InputDetector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace proto_clement {
+    public class InputDetector : MonoBehaviour {
+        public GameObject entryA;
+        public GameObject entryX;
+        public GameObject entryB;
+        public GameObject entryY;
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
